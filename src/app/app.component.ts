5b9f261e30d12cc2c596f62bd74a22c9abfbe514
Mcaua7 <matheuscaua0306@gmail.com>
2025-05-26 11:01:02 -0300

    Adicionando icones e começando a primeira tela

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
+import { HomePageComponent } from './home-page/home-page.component';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, HomePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router'
+
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
 export class HomePageComponent {
-
+    
 }
