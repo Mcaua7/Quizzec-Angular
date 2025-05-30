461d051fc911da94959ccd185932bd1f451f38ab
Mcaua7 <matheuscaua0306@gmail.com>
2025-05-30 09:23:06 -0300

    listando os quizes com ng for

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,11 +1,12 @@
 import { QuizDataService } from './../quiz-data.service';
 import { Component, OnInit } from '@angular/core';
 
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router'
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home-page',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
@@ -21,7 +22,7 @@ export class HomePageComponent {
     this.QuizDataService.getData().subscribe((data) => {
       //console.log(data)
       this.quizData = data
-      //console.log("Dados da requisição",this.quizData.record)
+      console.log("Dados da requisição",this.quizData.record)
       this.resultData = this.quizData.record[0]
       console.log("Resultado final : ",this.resultData)
     })
